7b329e5dcc9f8d41fcf55f0595f9663669142437
luiz vicente <luizvprodrigues@gmail.com>
2021-09-15 21:20:02 -0300

    magalu

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var receitasRouter = require('./routes/receitas');
+var receitas1Router = require('./routes/receitas1');
+var receitas2Router = require('./routes/receitas2');
+var receitas3Router = require('./routes/receitas3');
+var receitas4Router = require('./routes/receitas4')
 
 var app = express();
 
@@ -23,6 +27,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/receitas', receitasRouter);
+app.use('/receitas1', receitas1Router);
+app.use('/receitas2', receitas2Router);
+app.use('/receitas3', receitas3Router);
+app.use('/receitas4', receitas4Router);
 
 
 // catch 404 and forward to error handler
