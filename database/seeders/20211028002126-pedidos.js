90f9a665ba5019267fae5c626bc4fd7cae53e5fb
Luiz Vicente <luizvprodrigues@gmail.com>
2021-10-27 21:57:06 -0300

    seed

---
+++ database/seeders/20211028002126-pedidos.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('pedidos', [
+     {
+      produtos_id: 1,
+      status: 'aprovado'
+     }
+   ])
+  },
+  //id, created_at, updated_at,
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
