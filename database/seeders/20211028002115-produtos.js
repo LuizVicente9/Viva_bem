dad048e2915e0c1048514d7653c7112d8c4b5cdb
ultramarilia <ultramarilia@hotmail.com>
2021-11-04 21:37:40 -0300

    ta indo

--- database/seeders/20211028002115-produtos.js
+++ database/seeders/20211028002115-produtos.js
@@ -7,7 +7,20 @@ module.exports = {
       preco: '49,90', 
       descricao: 'Antioxidante Natural com 60 capsulas', 
       imagem: 'https://ibb.co/YLDK5fT', 
-      }], {});
+       },
+       {
+        nome_produro: 'Vitamina D',
+        preco: '59,90', 
+        descricao: 'Produto om 60 capsulas', 
+        imagem: 'https://ibb.co/0KDcv4X', 
+       },
+       {
+        nome_produro: 'Vitaminas A a Z',
+        preco: '89,90', 
+        descricao: 'Produto cgit aom 60 capsulas', 
+        imagem: 'https://ibb.co/cQLXMTV', 
+        }
+    ], {});
    },
 
   down: async (queryInterface, Sequelize) => {
